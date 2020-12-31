2f66723203863eacdc9d5edd672739a05697048a
Steven Terrana <steventerrana@gmail.com>
2020-12-31 18:05:57 -0500

    update code block styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,25 @@
 
 require('./src/assets/scss/init.scss');
 require('./src/assets/scss/admonitions.scss');
-require('./static/css/prismjs/theme.min.css');
+require('./src/assets/scss/code-blocks.scss')
+// require('./static/css/prismjs/theme.min.css');
+require('prismjs/themes/prism.css')
+require('@fontsource/fira-code')
+
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+    let dotHolder = document.createElement("div")
+    dotHolder.className = "dot-holder"
+    let colors = [ "red", "yellow", "green" ]
+    colors.forEach(color => {
+        let span = document.createElement("span")
+        span.className = `dot ${color}`
+        dotHolder.appendChild(span)
+    })
+
+    let codeBlocks = document.querySelectorAll("div.gatsby-highlight")
+    codeBlocks.forEach(block => {
+        console.log(block)
+        block.insertBefore(dotHolder.cloneNode(true), block.firstChild)
+        console.log(block)
+    })
+}
